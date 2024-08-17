c069b949171bf03682688625d429e216bf50e916
Colton Hicks <github@coltonhicks.com>
2024-08-16 17:48:49 -0700

    Refactored Structure views to use py3Dmol GridViewer when laying out multiple structures to use only one WebGL context to back them all. Added view for ConformerSearchResults. Added 2D image option for molecule viewer.

--- qcio/utils.py
+++ qcio/utils.py
@@ -9,7 +9,13 @@ from .models import Structure  # FileOutput,
 # Helper Structures
 water = Structure(
     symbols=["O", "H", "H"],
-    geometry=np.array([0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0, 1.0, 0.0]),
+    geometry=np.array(
+        [
+            [0.0253397, 0.01939466, -0.00696322],
+            [0.22889176, 1.84438441, 0.16251426],
+            [1.41760224, -0.62610794, -1.02954938],
+        ]
+    ),
     charge=0,
     multiplicity=1,
     connectivity=[(0, 1, 1.0), (0, 2, 1.0)],
